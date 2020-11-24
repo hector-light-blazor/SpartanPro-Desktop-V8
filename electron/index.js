0c15323f699a20d4be7cb2e53ee4de234964947f
Hector Chapa <roensebastian2015@gmail.com>
2020-11-24 13:40:46 -0600

    Making Minor changes

--- electron/index.js
+++ electron/index.js
@@ -17,7 +17,7 @@ function createWindow () {
       'http://localhost:4200/'
   )
   //win.loadFile('index.html')
-  win.webContents.openDevTools()
+  //win.webContents.openDevTools()
 }
 
 app.whenReady().then(createWindow)
