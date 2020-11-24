eb02b2c13d891f20731be415f6328294c6959e16
lrgvdc9-1-1 <hchapa@lrgvdc911.org>
2020-11-24 13:20:39 -0600

    Electron Folder and Contents

---
+++ electron/index.js
@@ -0,0 +1,35 @@
+const { app, BrowserWindow } = require('electron')
+
+function createWindow () {
+  const win = new BrowserWindow({
+    width: 1000,
+    height: 928,
+    frame: false,
+    backgroundColor: "#00ffffff",
+    hasShadow: true,
+    transparent: true,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+
+  win.loadURL(
+      'http://localhost:4200/'
+  )
+  //win.loadFile('index.html')
+  win.webContents.openDevTools()
+}
+
+app.whenReady().then(createWindow)
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow()
+  }
+})
